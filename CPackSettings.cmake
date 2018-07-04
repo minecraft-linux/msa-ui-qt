d1e4a067ec71d5947f4a1e7abb35062080c3a50c
MCMrARM <mcmrarm@gmail.com>
2018-07-04 20:22:17 +0200

    Debian package dependencies

--- CPackSettings.cmake
+++ CPackSettings.cmake
@@ -5,5 +5,6 @@ set(CPACK_PACKAGE_CONTACT "https://github.com/minecraft-linux/msa-manifest/issue
 set(CPACK_GENERATOR "TGZ;DEB")
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR};msa-ui-qt;msa-ui-qt;/")
 set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libqt5widgets5, libqt5webenginewidgets5")
 
 include(CPack)
