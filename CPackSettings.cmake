52aad600ea2770def04c8527a827e231a1ff1fff
MCMrARM <mcmrarm@gmail.com>
2018-11-22 23:11:43 +0100

    CPackSettings: Add xenial deps

--- CPackSettings.cmake
+++ CPackSettings.cmake
@@ -6,7 +6,11 @@ set(CPACK_PACKAGE_CONTACT "https://github.com/minecraft-linux/msa-manifest/issue
 set(CPACK_GENERATOR "TGZ;DEB")
 set(CPACK_INSTALL_CMAKE_PROJECTS "${CMAKE_CURRENT_BINARY_DIR};msa-ui-qt;msa-ui-qt;/")
 set(CPACK_OUTPUT_CONFIG_FILE CPackConfig.cmake)
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libqt5widgets5, libqt5webenginewidgets5")
+if (DEB_XENIAL_DEPENDENCIES)
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), qt59base")
+else()
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>=2.14), libqt5widgets5, libqt5webenginewidgets5")
+endif()
 set(CPACK_DEBIAN_PACKAGE_VERSION "${BUILD_TIMESTAMP}-${MANIFEST_GIT_COMMIT_HASH}")
 
 include(CPack)
